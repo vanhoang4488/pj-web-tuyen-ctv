8e3a1fcf67d4ec5283a4df6cb74dbe325e018cac
vanhoang48 <vanhoang48.nguyen@gmail.com>
2022-09-14 17:42:32 +0700

    5

--- apply-upload/src/main/java/com/os/upload/controller/UploadWordController.java
+++ apply-upload/src/main/java/com/os/upload/controller/UploadWordController.java
@@ -1,6 +1,9 @@
 package com.os.upload.controller;
 
 import com.os.result.ResultEntity;
+import com.os.upload.service.WordService;
+import lombok.RequiredArgsConstructor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +14,10 @@ import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/docx")
+@RequiredArgsConstructor
 public class UploadWordController {
 
+    private final WordService wordService;
 
     @RequestMapping("/createArticleByWordDoc")
     public ResultEntity createArticleByWordDoc(HttpServletRequest request,
@@ -21,7 +26,13 @@ public class UploadWordController {
         // todo kiểm tra định file và content-type.
 
         // phân tích file
+        try{
+            XWPFDocument document = new XWPFDocument(multipartFile.getInputStream());
 
-        return ResultEntity.success().build();
+            return wordService.createArticleByWordDoc(document);
+        }
+        catch (Exception e){
+            return ResultEntity.failed().build();
+        }
     }
 }
