3c7582c01216668abd65076a090116789a4e6492
vanhoang4488 <vanhoang4488.nguyen@gmail.com>
2023-11-08 00:17:16 +0700

    '11082023'

--- v.3/web_tuyen_ctv/user/src/main/java/vanhoang/project/controller/UserController.java
+++ v.3/web_tuyen_ctv/user/src/main/java/vanhoang/project/controller/UserController.java
@@ -1,6 +1,8 @@
 package vanhoang.project.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
+    private final DiscoveryClient discoveryClient;
+    private final Environment environment;
+
     @GetMapping("/users")
     public ResponseEntity<String> findUsers() {
+        System.out.println(discoveryClient.getServices());
+        System.out.println(environment.get);
         return ResponseEntity.ok("ok");
     }
 }
